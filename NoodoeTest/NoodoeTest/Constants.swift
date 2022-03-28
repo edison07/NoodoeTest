2324ebeeba19ffcbf5583d6748c2a5db0a02e939
Edison <edisonlin@pixnet.tw>
2022-03-28 10:52:11 +0800

    feat: 將 request url 存在 constants

---
+++ NoodoeTest/NoodoeTest/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  NoodoeTest
+//
+//  Created by edisonlin on 2022/3/28.
+//
+
+import Foundation
+
+struct Constants {
+    static let url = "https://watch-master-staging.herokuapp.com/api/login"
+}
